1a3798e609ee0f448233fb4a39f2ff247b7b5894
tema <tema@vf8.ru>
2015-06-16 17:22:39 +0300

    Comments added

--- count_words.c
+++ count_words.c
@@ -1,3 +1,12 @@
+// 1. program to read a sentence of text from the user, character by character
+// 2. count the number of words in that sentence 
+// 3. if there are more than one consecutive spaces then ignore them
+// 4. terminate when the end of a sentence is reached (denoted by a period ’.’).
+// 5. print the final number of words encountered.
+// 6. ignore anything that is not a letter so it will count only the words.
+// Hint: It might be a good idea to use the numerical values from the ASCII table.
+
+
 #include <stdio.h>
 
 int main() {
